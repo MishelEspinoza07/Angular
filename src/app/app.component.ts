8e483cf06e80601fd00d337d67cd083e92854029
MishelEspinoza07 <102001751+MishelEspinoza07@users.noreply.github.com>
2024-09-25 20:46:50 -0400

    [25/09/2024]Desestructuracion, Preed operator and Rest operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,16 +24,34 @@ export class AppComponent {
     age: 20
   }
 
+  students:number[]=[1,2,3,4,5,6]
+  parents:number[]=[7,8,9,10]
+
+
   constructor(){
-    console.log('substract', this.substrack(8,4))
-    console.log('MAP:', this.animals.map((animal)=> (animal + ' new')))
-    console.log('FOREACH:', this.animals.forEach((animal)=> (animal + ' new')))
-    console.log('FIND:', this.animals.find((animal)=> animal==='z'))
-    console.log('FILTER:', this.animals.filter((animal)=> animal==='y'))
-    console.log('INDEXOF:', this.animals.indexOf('c'))
+    const {name, age} = this.person
+    console.log('desestructuracion:', name, age)
+    let both = [...this.students, ...this.parents]
+    console.log('spreed operator:', both)
+    console.log('REST operator:', this.sum(2,4,6))
+
+
+    // console.log('substract', this.substrack(8,4))
+    // console.log('MAP:', this.animals.map((animal)=> (animal + ' new')))
+    // console.log('FOREACH:', this.animals.forEach((animal)=> (animal + ' new')))
+    // console.log('FIND:', this.animals.find((animal)=> animal==='z'))
+    // console.log('FILTER:', this.animals.filter((animal)=> animal==='y'))
+    // console.log('INDEXOF:', this.animals.indexOf('c'))
   }
 
-  public sum(num1: number, num2: number): number{
+  public sum (...persons:number[]){
+    // return persons[0] + persons[1]
+    return persons.reduce(
+      (acumulador, valorActual)=> (acumulador + valorActual), 10)
+  }
+  
+
+  public sum2(num1: number, num2: number): number{
     return num1 + num2;
   }
 
@@ -44,7 +62,7 @@ export class AppComponent {
     const people: number[] = [1,2,3,4,5,6,7,8];
     for(let i = 0; i < people.length; i++){
       if(people[i] % 2 == 0){
-        console.log('person =', people[i]);
+        // console.log('person =', people[i]);
       }
     }
   }
