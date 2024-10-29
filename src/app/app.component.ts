26b73e4110f1ebbf712436730863640be67c2a54
MishelEspinoza07 <102001751+MishelEspinoza07@users.noreply.github.com>
2024-10-29 19:13:30 -0400

    [14-10-2024] Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,8 @@ import { AppColorsDirective } from "./app-colors.directive";
 import { CreateHtmlDirective } from "./create-html.directive";
 import { PurePipe } from "./pure.pipe";
 import { ImpurePipe } from "./impure.pipe";
+import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from "@angular/material/button";
 
 interface Person {
   name: string;
@@ -29,7 +31,9 @@ interface Person {
     AppColorsDirective,
     CreateHtmlDirective,
     PurePipe,
-    ImpurePipe
+    ImpurePipe,
+    MatCardModule,
+    MatButtonModule
   ],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
